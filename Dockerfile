f9c4c295c7a0afc8d1ce4768294009f0fceb3507
rahul <rahulgoyal4u@gmail.com>
2022-05-19 20:48:16 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:11
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+ENV classpath=com.goyal.demoapp.App
+ENV classargs=
+ENTRYPOINT ["java","-cp","/app.jar", ${classpath}, ${classargs}]
---
+++ src/main/java/com/goyal/demoapp/App.java
@@ -0,0 +1,13 @@
+package com.goyal.demoapp;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Passed Arguments: " + Arrays.toString(args));
+    }
+}
